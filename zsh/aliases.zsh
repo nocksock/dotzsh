2a6fcd24a82dd41bb698bd0c5d207562fd6828d7
Nils Riedemann <nils@riedemann.co>
2015-09-01 13:54:13 +0200

    updates

--- zsh/aliases.zsh
+++ zsh/aliases.zsh
@@ -12,3 +12,26 @@ function tmn() {
 }
 
 alias dsend='open -a Transmit --hide --background'
+
+git-grouped-log () {
+  while read -r -u 9 since name
+  do
+    until=$(date -j -v+1d -f '%Y-%m-%d' $since +%Y-%m-%d)
+
+    echo "$since $name"
+    echo
+
+    GIT_PAGER=cat git log             \
+      --no-merges                     \
+      --committer="$name"             \
+      --since="$since 00:00:00 +0000" \
+      --until="$until 00:00:00 +0000" \
+      --format='  * [%h] %s'
+
+    echo
+  done 9< <(git log --no-merges --format=$'%cd %cn' --date=short | sort --unique --reverse)
+}
+
+function codekit() {
+	open $1 -a CodeKit
+}
