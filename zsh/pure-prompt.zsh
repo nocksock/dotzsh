3ead574dd82029f423d610caadeb9bac073a4060
Nils Riedemann <nils@bleepbloop.studio>
2021-03-06 08:25:05 +0100

    removing pure prompt in favor of starship

--- zsh/pure-prompt.zsh
+++
@@ -1,9 +0,0 @@
-# Pure Prompt
-# https://github.com/sindresorhus/pure#getting-started 
-autoload -U promptinit; promptinit
-PURE_CMD_MAX_EXEC_TIME=10
-prompt pure
-
-# Autocomplete
-autoload -Uz compinit
-compinit
